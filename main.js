94022a4d382abccb3823a3bac9f8ccfd38f5ba38
Laura Tavares <144948480+LauraTavares2323@users.noreply.github.com>
2023-12-06 10:56:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,311 @@
+palavraTexto = document.querySelectorAll(".forte")
+// horror - caixa e audio
+
+// caixa
+abrir1 = document.getElementById("palavra1")
+fechar1 = document.getElementById("fechar1")
+caixa1 = document.getElementById("horror")
+
+abrir1.addEventListener("click", (e) => {
+    caixa1.style.visibility = "visible"
+    palavraTexto.style.visibility = "visible"
+})
+
+fechar1.addEventListener("click", (e) => {
+    caixa1.style.visibility = "hidden"
+})
+
+
+// audio 
+
+audio1 = document.getElementById("audio1")
+imagem1 = document.getElementById("imagem1")
+
+imagem1.addEventListener('mouseover', function () {
+    audio1.play();
+})
+
+imagem1.addEventListener('mouseleave', function () {
+    audio1.pause();
+    audio1.currentTime = 0;
+})
+
+
+// bulglary - caixa e audio
+
+// caixa
+abrir2 = document.getElementById("palavra2")
+fechar2 = document.getElementById("fechar2")
+caixa2 = document.getElementById("burglary")
+
+abrir2.addEventListener("click", (e) => {
+    caixa2.style.visibility = "visible"
+})
+
+fechar2.addEventListener("click", (e) => {
+    caixa2.style.visibility = "hidden"
+})
+
+// audio 
+
+audio2 = document.getElementById("audio2")
+imagem2 = document.getElementById("imagem2")
+
+imagem2.addEventListener('mouseover', function () {
+    audio2.play();
+})
+
+imagem2.addEventListener('mouseleave', function () {
+    audio2.pause();
+    audio2.currentTime = 0;
+})
+
+
+// Crow - caixa e audio
+
+// caixa
+abrir3 = document.getElementById("palavra3")
+fechar3 = document.getElementById("fechar3")
+caixa3 = document.getElementById("crow")
+
+abrir3.addEventListener("click", (e) => {
+    caixa3.style.visibility = "visible"
+})
+
+fechar3.addEventListener("click", (e) => {
+    caixa3.style.visibility = "hidden"
+})
+
+// audio 
+
+audio3 = document.getElementById("audio3")
+imagem3 = document.getElementById("imagem3")
+
+imagem3.addEventListener('mouseover', function () {
+    audio3.play();
+})
+
+imagem3.addEventListener('mouseleave', function () {
+    audio3.pause();
+    audio3.currentTime = 0;
+})
+
+// Flautist - caixa e audio
+
+// caixa
+abrir4 = document.getElementById("palavra4")
+fechar4 = document.getElementById("fechar4")
+caixa4 = document.getElementById("flautist")
+
+abrir4.addEventListener("click", (e) => {
+    caixa4.style.visibility = "visible"
+})
+
+fechar4.addEventListener("click", (e) => {
+    caixa4.style.visibility = "hidden"
+})
+
+// audio 
+
+audio4 = document.getElementById("audio4")
+imagem4 = document.getElementById("imagem4")
+
+imagem4.addEventListener('mouseover', function () {
+    audio4.play();
+})
+
+imagem4.addEventListener('mouseleave', function () {
+    audio4.pause();
+    audio4.currentTime = 0;
+})
+
+// Air conditioning  - caixa e audio
+
+// caixa
+abrir5 = document.getElementById("palavra5")
+fechar5 = document.getElementById("fechar5")
+caixa5 = document.getElementById("airconditioning")
+
+abrir5.addEventListener("click", (e) => {
+    caixa5.style.visibility = "visible"
+})
+
+fechar5.addEventListener("click", (e) => {
+    caixa5.style.visibility = "hidden"
+})
+
+// audio 
+
+audio5 = document.getElementById("audio5")
+imagem5 = document.getElementById("imagem5")
+
+imagem5.addEventListener('mouseover', function () {
+    audio5.play();
+})
+
+imagem5.addEventListener('mouseleave', function () {
+    audio5.pause();
+    audio5.currentTime = 0;
+})
+
+// Overwhelmed  - caixa e audio
+
+// caixa
+abrir6 = document.getElementById("palavra6")
+fechar6 = document.getElementById("fechar6")
+caixa6 = document.getElementById("overwhelmed")
+
+abrir6.addEventListener("click", (e) => {
+    caixa6.style.visibility = "visible"
+})
+
+fechar6.addEventListener("click", (e) => {
+    caixa6.style.visibility = "hidden"
+})
+
+// audio 
+
+audio6 = document.getElementById("audio6")
+imagem6 = document.getElementById("imagem6")
+
+imagem6.addEventListener('mouseover', function () {
+    audio6.play();
+})
+
+imagem6.addEventListener('mouseleave', function () {
+    audio6.pause();
+    audio6.currentTime = 0;
+})
+
+
+// Library - caixa e audio
+
+// caixa
+abrir7 = document.getElementById("palavra7")
+fechar7 = document.getElementById("fechar7")
+caixa7 = document.getElementById("library")
+
+abrir7.addEventListener("click", (e) => {
+    caixa7.style.visibility = "visible"
+})
+
+fechar7.addEventListener("click", (e) => {
+    caixa7.style.visibility = "hidden"
+})
+
+// audio 
+
+audio7 = document.getElementById("audio7")
+imagem7 = document.getElementById("imagem7")
+
+imagem7.addEventListener('mouseover', function () {
+    audio7.play();
+})
+
+imagem7.addEventListener('mouseleave', function () {
+    audio7.pause();
+    audio7.currentTime = 0;
+})
+
+// Strawberry - caixa e audio
+
+// caixa
+abrir8 = document.getElementById("palavra8")
+fechar8 = document.getElementById("fechar8")
+caixa8 = document.getElementById("strawberry")
+
+abrir8.addEventListener("click", (e) => {
+    caixa8.style.visibility = "visible"
+})
+
+fechar8.addEventListener("click", (e) => {
+    caixa8.style.visibility = "hidden"
+})
+
+// audio 
+
+audio8 = document.getElementById("audio8")
+imagem8 = document.getElementById("imagem8")
+
+imagem8.addEventListener('mouseover', function () {
+    audio8.play();
+})
+
+imagem8.addEventListener('mouseleave', function () {
+    audio8.pause();
+    audio8.currentTime = 0;
+})
+
+// walk - caixa e audio
+
+// caixa
+abrir9 = document.getElementById("palavra9")
+fechar9 = document.getElementById("fechar9")
+caixa9 = document.getElementById("walk")
+
+abrir9.addEventListener("click", (e) => {
+    caixa9.style.visibility = "visible"
+})
+
+fechar9.addEventListener("click", (e) => {
+    caixa9.style.visibility = "hidden"
+})
+
+// audio 
+
+audio9 = document.getElementById("audio9")
+imagem9 = document.getElementById("imagem9")
+
+imagem9.addEventListener('mouseover', function () {
+    audio9.play();
+})
+
+imagem9.addEventListener('mouseleave', function () {
+    audio9.pause();
+    audio9.currentTime = 0;
+})
+
+// without - caixa e audio
+
+// caixa
+abrir10 = document.getElementById("palavra10")
+fechar10 = document.getElementById("fechar10")
+caixa10 = document.getElementById("without")
+
+abrir10.addEventListener("click", (e) => {
+    caixa10.style.visibility = "visible"
+})
+
+fechar10.addEventListener("click", (e) => {
+    caixa10.style.visibility = "hidden"
+})
+
+// audio 
+
+audio10 = document.getElementById("audio10")
+imagem10 = document.getElementById("imagem10")
+
+imagem10.addEventListener('mouseover', function () {
+    audio10.play();
+})
+
+imagem10.addEventListener('mouseleave', function () {
+    audio10.pause();
+    audio10.currentTime = 0;
+})
+
+
+// texto aparecendo
+
+let texto = document.getElementById("texto")
+let botao = document.getElementById("botao")
+let fechar = document.getElementById("fechar")
+
+botao.addEventListener('click', (e) => {
+    texto.style.display = "block"
+})
+
+
+fechar.addEventListener('click', (e) => {
+    texto.style.display = "none"
+})
